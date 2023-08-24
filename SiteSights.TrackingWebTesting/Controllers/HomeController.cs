249aefdfd45d14e52825dfdfcec52a08a2d674e1
Marvin Drude <marvin@gidd.io>
2023-08-24 02:09:49 +0200

    Commit first major version

---
+++ SiteSights.TrackingWebTesting/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteSights.Tracking.Common;
+using SiteSights.TrackingWebTesting.Models;
+using System.Diagnostics;
+
+namespace SiteSights.TrackingWebTesting.Controllers {
+
+    public class HomeController : Controller {
+
+        private readonly ISiteSightsTracking _tracking;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ISiteSightsTracking tracking) {
+            _logger = logger;
+            _tracking = tracking;
+        }
+
+        public async Task<IActionResult> Index() {
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
